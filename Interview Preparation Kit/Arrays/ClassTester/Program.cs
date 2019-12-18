80ef3c13cf3bd285c4956d48ccf54fd09ec90691
JiranKurian <jirankuria@gmail.com>
2019-12-18 17:54:00 +0530

    File structure completed
    Some sample code is also added

---
+++ Interview Preparation Kit/Arrays/ClassTester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Arrays;
+
+namespace ClassTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // D2ArrayDS.Main();
+            // ArraysLeftRotation.Main();
+            // NewYearChaos.Main();
+            MinimumSwaps2.Main();
+        }
+    }
+}
